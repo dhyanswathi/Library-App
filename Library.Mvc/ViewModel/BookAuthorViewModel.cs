3ecf13b0eba1196af3fe87b238e077b46447f6ab
swathi <swathysidharthan@gmail.com>
2023-02-02 09:04:57 +0100

    add bookauthorviewmodel

---
+++ Library.Mvc/ViewModel/BookAuthorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Library.Mvc.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Library.Mvc.ViewModel
+{
+    public class BookAuthorViewModel
+    {
+        public List<Book>? Books { get; set; }
+        public SelectList? Authors { get; set; }
+        public string? BookAuthor { get; set; }
+        public string? SearchString { get; set; }
+    }
+}
